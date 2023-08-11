e261388d95228f32447eb3705a4010022cc84603
Alex8804 <128159385+Alex8804@users.noreply.github.com>
2023-08-11 12:27:21 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const keys = {
+  lsFeedback: 'feedback-form-state',
+};
+const lastFeedback = JSON.parse(localStorage.getItem(keys.lsFeedback)) ?? {};
+
+updateFeedback();
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onSubmit);
+
+function onFormInput(evt) {
+  const { email, message } = evt.currentTarget.elements;
+
+  const feedback = {
+    lastEmail: `${email.value}`,
+    lastMassege: `${message.value}`,
+  };
+
+  localStorage.setItem(keys.lsFeedback, JSON.stringify(feedback));
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const { email, message } = evt.currentTarget.elements;
+  console.log(`Email: ${email.value}, Message: ${message.value}`);
+
+  form.reset();
+  localStorage.removeItem(keys.lsFeedback);
+}
+
+function updateFeedback() {
+  form.email.value = lastFeedback.lastEmail || '';
+  form.message.textContent = lastFeedback.lastMassege || '';
+}
