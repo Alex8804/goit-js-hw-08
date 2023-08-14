3b94e9d1096be0debf58a6858498550b0ab026e8
Alex8804 <128159385+Alex8804@users.noreply.github.com>
2023-08-14 18:50:36 +0200

    task-3 update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,36 @@
 import throttle from 'lodash.throttle';
-
 const form = document.querySelector('.feedback-form');
-
 const keys = {
   lsFeedback: 'feedback-form-state',
 };
+let lastFeedback = JSON.parse(localStorage.getItem(keys.lsFeedback)) || {};
 
-const lastFeedback = JSON.parse(localStorage.getItem(keys.lsFeedback)) ?? {};
+function updateFeedback() {
+  const { email, message } = form.elements;
+  email.value = lastFeedback.lastEmail || '';
+  message.value = lastFeedback.lastMessage || '';
+}
 
-updateFeedback();
+const throttledUpdate = throttle(updateFeedback, 500);
 
-form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('input', onFormInput);
 form.addEventListener('submit', onSubmit);
 
 function onFormInput(evt) {
   const { email, message } = evt.currentTarget.elements;
-
-  const feedback = {
-    lastEmail: `${email.value}`,
-    lastMassege: `${message.value}`,
+  lastFeedback = {
+    lastEmail: email.value,
+    lastMessage: message.value,
   };
-
-  localStorage.setItem(keys.lsFeedback, JSON.stringify(feedback));
+  localStorage.setItem(keys.lsFeedback, JSON.stringify(lastFeedback));
+  throttledUpdate();
 }
-
 function onSubmit(evt) {
   evt.preventDefault();
-
-  console.log(JSON.parse(localStorage.getItem(keys.lsFeedback)) ?? {});
-
-  updateFeedback();
+  console.log(lastFeedback);
   form.reset();
   localStorage.removeItem(keys.lsFeedback);
+  lastFeedback = {};
+  updateFeedback();
 }
-
-function updateFeedback() {
-  form.email.value = lastFeedback.lastEmail || '';
-  form.message.value = lastFeedback.lastMassege || '';
-}
+updateFeedback();
